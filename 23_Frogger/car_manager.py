4ba6b8f9aaa7e7cc9433bdab56e2b75ec0ea1f08
LSFL <flammdumbfox@gmail.com>
2023-03-23 00:58:32 -0300

    Primitive car generator working.

--- 23_Frogger/car_manager.py
+++ 23_Frogger/car_manager.py
@@ -1,7 +1,48 @@
+from turtle import Turtle
+from random import choice, randint
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 600
+SPAWNABLE_AREA_DIFFERENCE = 80
 
 class CarManager:
-    pass
+    
+    def __init__(self):
+        
+        self.car_list = list()
+        
+        
+    def generate_car(self) -> None:
+        
+        car = Turtle()
+        
+        # Car visuals
+        car.penup()
+        car.shape("square")
+        car.shapesize(stretch_len = 3.0, stretch_wid = 1.0)
+        car.color(choice(COLORS))
+        car.speed(0)
+        
+        # Car settings (mechanics)
+        car.speed = -MOVE_INCREMENT
+        car.setheading(180)
+        car.setpos(360, randint(
+            - WINDOW_HEIGHT / 2 + SPAWNABLE_AREA_DIFFERENCE,
+            WINDOW_HEIGHT / 2 - SPAWNABLE_AREA_DIFFERENCE)
+        )
+        
+        self.car_list.append(car)
+        
+    def process_car_movement(self) -> None:
+        for car in self.car_list:
+            car.setx(car.xcor() + car.speed)
+
+            # Detect when out of bounds
+            if car.xcor() >= 360 or car.xcor() <= -360:
+                car.clear()
+                car.hideturtle()
+                self.car_list.remove(car)
--- 23_Frogger/main.py
+++ 23_Frogger/main.py
@@ -4,11 +4,27 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
 
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
+def main():
+    
+    # Setting up the screen
+    screen = Screen()
+    screen.setup(width = 600, height = 600)
+    screen.colormode(255)
+    screen.bgcolor("white")
+    screen.title("2-digit budget Frogger clone")
+    screen.tracer(0)
+    
+    # Setting up the car movements
+    car_manager = CarManager()
+
+    game_is_on = True
+    while game_is_on:
+        time.sleep(0.1)
+        car_manager.generate_car()
+        car_manager.process_car_movement()
+        screen.update()
+    
+    screen.exitonclick()
+    
+main()
