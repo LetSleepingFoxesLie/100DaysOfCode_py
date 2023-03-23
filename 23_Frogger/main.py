419883693c58cfd8f12c1e1de47770bfd9634405
LSFL <flammdumbfox@gmail.com>
2023-03-23 02:01:11 -0300

    Added turtle movement. 36:05

--- 23_Frogger/main.py
+++ 23_Frogger/main.py
@@ -18,9 +18,17 @@ def main():
     # Setting up the car movements
     car_manager = CarManager()
 
+    # Setting up the player
+    player = Player()
+    
+    # Listening for events
+    screen.listen()
+    screen.onkey(fun = player.move_backward, key = "s")
+    screen.onkey(fun = player.move_forward, key = "w")
+    
     game_is_on = True
     while game_is_on:
-        time.sleep(0.1)
+        time.sleep(0.016)
         car_manager.generate_car()
         car_manager.process_car_movement()
         screen.update()
--- 23_Frogger/player.py
+++ 23_Frogger/player.py
@@ -6,4 +6,26 @@ FINISH_LINE_Y = 280
 
 
 class Player(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        
+        # Turtle's variables
+        self.speed = 0
+        
+        # Setup the turtle's visuals!
+        self.penup()
+        self.seth(90)
+        self.shape("turtle")
+        self.color("black")
+        self.setpos(STARTING_POSITION)
+        
+    def move_forward(self):
+        self.sety(self.ycor() + MOVE_DISTANCE)
+    
+    def move_backward(self):
+        if self.ycor() <= -280:
+            return
+        self.sety(self.ycor() - MOVE_DISTANCE)
+        
     pass
