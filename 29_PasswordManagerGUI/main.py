9b6315727079e8f80fa2aa52c1850859a53555bd
LSFL <flammdumbfox@gmail.com>
2023-03-30 23:48:43 -0300

    Added "save to file" functionality.

--- 29_PasswordManagerGUI/main.py
+++ 29_PasswordManagerGUI/main.py
@@ -1,9 +1,23 @@
 from tkinter import *
 
+FONT_NAME = "Bahnschrift"
+USUAL_EMAIL = "someonesemail@provider.com"
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+# Get all relevant information and save it to a file
+def save_information_to_file():
+    try:
+        with open(r"29_PasswordManagerGUI/passwords.txt", "a") as f:
+            website = entry_website.get()
+            username = entry_name.get()
+            password = entry_password.get()
+            f.write(f"{website} | {username} | {password}")
+    except OSError:
+        with open(r"29_PasswordManagerGUI/passwords.txt", "w"):
+            save_information_to_file()
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -25,6 +39,37 @@ canvas.create_image(
     image = image_logo
 )
 
-canvas.pack()
+canvas.grid(
+    row = 0, column = 1
+)
+
+## Labels
+label_website = Label(text = "Website:", font = (FONT_NAME, 10, "normal"), bg = "white")
+label_name = Label(text = "Email/Username:", font = (FONT_NAME, 10, "normal"), bg = "white")
+label_password = Label(text = "Password:", font = (FONT_NAME, 10, "normal"), bg = "white")
+
+label_website.grid(row = 1, column = 0)
+label_name.grid(row = 2, column = 0)
+label_password.grid(row = 3, column = 0)
+
+## Entries
+entry_website = Entry()
+entry_website.focus() # So less clicks are needed. Magic!
+entry_name = Entry()
+entry_name.insert(0, USUAL_EMAIL)
+entry_password = Entry()
+
+entry_website.grid(row = 1, column = 1, columnspan = 2, sticky = "EW", padx = (10, 10))
+entry_name.grid(row = 2, column = 1, columnspan = 2, sticky = "EW", padx = (10, 10))
+entry_password.grid(row = 3, column = 1, columnspan = 2, sticky = "EW", padx = (10, 10))
+
+## Buttons
+button_generate_pw = Button(text = "Generate password", font = (FONT_NAME, 9, "normal"))
+button_generate_pw.grid(row = 3, column = 2, sticky = "EW", padx = (10, 10))
+
+button_add_to_file = Button(text = "Add to file", font = (FONT_NAME, 9, "normal"),
+                            command = save_information_to_file)
+button_add_to_file.grid(row = 4, column = 1, columnspan = 2, sticky = "EW", padx = (10, 10))
+
 
 window.mainloop()
